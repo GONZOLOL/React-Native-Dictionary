020e1c4597e2e9d305e26f4804fac8cb5ebadfba
GONZOLOL <glerdie489@g.educaand.es>
2023-03-21 14:09:09 +0100

    main component app creation

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Home } from "./components/Home";
+import { RecentLinks } from "./components/RecentLinks";
+import { About } from "./components/About";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "About") {
+              iconName = "help";
+            } else if (route.name === "Recent Links") {
+              iconName = "link";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#009688",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Recent Links" component={RecentLinks} />
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="About" component={About} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
